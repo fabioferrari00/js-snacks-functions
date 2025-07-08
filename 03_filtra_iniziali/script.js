784b6cda08634aba4d5910bca29118d50c3fbbdb
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-08 13:33:37 +0200

    [upd] terza funzione

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const findLetter = (array, letter) => {
+
+  let wordWithLetter = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].charAt(0) === letter) {
+      wordWithLetter.push(array[i]);
+    }
+  }
+
+  return wordWithLetter;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(findLetter(names, 'L'))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
