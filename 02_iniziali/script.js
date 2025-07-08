f3625ffedc43a84e0c6b514509709f4255bfa6a1
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-08 13:26:29 +0200

    [upd] seconda funzione

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const findFirstCharacter = (array) => {
 
+  let firstCharacters = [];
+  for (let i = 0; i < array.length; i++) {
+    let firstCharacter = array[i].charAt(0);
+
+    firstCharacters.push(firstCharacter);
+
+  }
+  return firstCharacters;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(findFirstCharacter(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
