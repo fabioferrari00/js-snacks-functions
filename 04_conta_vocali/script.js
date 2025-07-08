0aae3614a1c871f3d6ac5205e261b237de472fbc
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-08 13:39:35 +0200

    [upd] quarta funzione

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,26 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const findVocals = (string) => {
+  let vocals = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() === 'a') {
+      vocals++;
+    } else if (string[i].toLowerCase() === 'e') {
+      vocals++;
+    } else if (string[i].toLowerCase() === 'i') {
+      vocals++;
+    } else if (string[i].toLowerCase() === 'o') {
+      vocals++;
+    } else if (string[i].toLowerCase() === 'u') {
+      vocals++;
+    }
+  }
+  return vocals;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(findVocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
